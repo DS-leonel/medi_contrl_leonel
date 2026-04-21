a58757dae724e20cc38f43f1714348246f001462
Neid-09 <neiderqm09@gmail.com>
2026-04-21 16:16:50 -0500

    feat: integrar passport-jwt strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { AppJwtModule } from '../app-jwt/app-jwt.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, AppJwtModule],
+  imports: [UsersModule, AppJwtModule, 
+    PassportModule.register({ defaultStrategy: 'jwt' })
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AppJwtModule, PassportModule],
 })
 export class AuthModule {}
