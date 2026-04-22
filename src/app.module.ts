a5803c54702fb5cefe2e6913994dc091a7264e37
aplicacionJava <jlenriquez24@itp.edu.co>
2026-04-22 08:55:39 -0500

    registrar módulo de Citas en la aplicación principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { DatabaseModule } from './database/database.module';
 import { EnvModule } from './config/env/env.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppJwtModule } from './modules/app-jwt/app-jwt.module';
+import { MedicosModule } from './modules/medicos/medicos.module';
 import { CitasModule } from './modules/citas/citas.module';
 
 @Module({
-  imports: [DatabaseModule,EnvModule, AuthModule, AppJwtModule, CitasModule],
+  imports: [DatabaseModule, EnvModule, AuthModule, AppJwtModule, MedicosModule, CitasModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/citas/citas.module.ts
+++ src/modules/citas/citas.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CitasService } from './citas.service';
 import { CitasController } from './citas.controller';
+import { Cita } from './entities/cita.entity';
+import { Medico } from '../medicos/entities/medico.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cita, Medico])],
   controllers: [CitasController],
   providers: [CitasService],
+  exports: [CitasService],
 })
 export class CitasModule {}
