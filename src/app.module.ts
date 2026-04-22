c0ee991340c7f9de8f25058fde16337e887029b0
Neid-09 <neiderqm09@gmail.com>
2026-04-22 10:25:24 -0500

    fix: agrega modulo pacientes que faltaba en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { EnvModule } from './config/env/env.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppJwtModule } from './modules/app-jwt/app-jwt.module';
 import { MedicosModule } from './modules/medicos/medicos.module';
+import { PacientesModule } from './modules/pacientes/pacientes.module';
 
 @Module({
-  imports: [DatabaseModule,EnvModule, AuthModule, AppJwtModule, MedicosModule],
+  imports: [DatabaseModule,EnvModule, AuthModule, AppJwtModule, MedicosModule, PacientesModule],
   controllers: [AppController],
   providers: [AppService],
 })
